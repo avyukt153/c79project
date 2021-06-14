dbd31a0c8a71d66a2c4d3c7659af7e60b7e0043b
avyukt153 <71768477+avyukt153@users.noreply.github.com>
2021-06-14 15:16:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+menu_list_array = ["Veg Margherita Pizza",
+"chicken tandoori pizza",
+"veg supreme pizza" ,
+"panner tikka pizza",
+"deluxe veggie pizza",
+"veg  extravagna pizza"];
+function getmenu(){
+var htmldata;menu_list_array
+document.getElementById("menu_list")  
+
+
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+
+
+}
+
